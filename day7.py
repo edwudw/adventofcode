795f0fab95f31b9736e457df7de3be8eaba23d8e
Edward Webb <edward.webb9@outlook.com>
2022-12-07 22:49:05 +1100

    I fixed itgit add .!

--- day7.py
+++ day7.py
@@ -15,39 +15,47 @@ def generateTree(name, level):
     lineNum += 1
     children = {}
     total_size = 0
-    # print((" " * (level - 1)) + name + ' lineNum: ' + str(lineNum))
+    print((" " * (level - 1)) + name)
     while curr != "$ cd .." and lines:
         curr_split = curr.split(' ', 1)
+        print('currName: ' + name + ': ' + str(curr_split))
         if curr[:len("$ cd")] == "$ cd":
             # is a directory
             dir_name = curr[len("$ cd "):]
 
             children[dir_name] = generateTree(dir_name, level+1)
             total_size += children[dir_name].size
-            print('total so far: ' + str(children[dir_name].size))
-            print('TOTAL SIZE: ' + str(total_size))
-        if curr_split[0] != '$' and curr_split[0] != 'dir':
+            #print('total so far: ' + str(children[dir_name].size))
+            #print('TOTAL SIZE: ' + str(total_size))
+        elif curr_split[0] != '$' and curr_split[0] != 'dir':
             # is a file
             file_name = curr_split[1]
             file_size = int(curr_split[0])
             children[file_name] = Tree(file_name, "file", file_size, {})
-            print(file_size)
+            #print(file_size)
             total_size += file_size
-            print('total so far: ' + str(total_size))
+            #print('total so far: ' + str(total_size))
         if not lines:
             break
         curr = lines[0]
         lines = lines[1:]
         lineNum += 1
     
+    if len(lines) == 0:
+        curr_split = curr.split(' ', 1)
+        print('currName: ' + name + ': ' + str(curr_split))
+        if curr_split[0] != '$' and curr_split[0] != 'dir':
+            # is a file
+            file_name = curr_split[1]
+            file_size = int(curr_split[0])
+            children[file_name] = Tree(file_name, "file", file_size, {})
+            #print(file_size)
+            total_size += file_size
     ## print(level)
-    if lines and lines[0] == "$ cd ..":
-        lines = lines[1:] # first element will be cd ..
-        lineNum += 1
-    # print((" " * (level - 1)) + "ending: " + name + ', ' + str(total_size) + str(total_size < 100000))
+    print((" " * (level - 1)) + "ending: " + name + ', ' + str(total_size))
     
     ## print('line: ' + str(lineNum))
-    return Tree(name, "dir", 0, children)
+    return Tree(name, "dir", total_size, children)
 
 def fixSize(tree):
     total = 0
@@ -62,7 +70,7 @@ total = 0
 def readTree(tree):
     global total
     if tree.type == 'dir' and tree.size <= 100000:
-        print(tree.name + ', ' + str(tree.size))
+        #print(tree.name + ', ' + str(tree.size))
         total += tree.size
     for child in tree.children:
         readTree(tree.children[child])
@@ -73,6 +81,6 @@ if __name__ == "__main__":
         lines = f.read().splitlines()
     lines = lines[1:]
     main = generateTree("/", 1)
-    fixSize(main)
+    # # fixSize(main)
     readTree(main)
     print(total)
